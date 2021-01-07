2f66fbab337b5f3e2ad37179937571e625c4fa4b
Sanket Sonawane <sanketssonawane10@gmail.com>
2021-01-07 20:32:48 +0530

    commited final

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+# Nasa Api App
+
+
+(1) Clone the project by this command
+
+
+
+
+(1) this is home url -> u will get todays astronomy image with explanation
+
+http://localhost:3000/
+
+
+(2) Just pick a date to see particular date astronomy with image and explanation
+
+
---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
+const path = require('path');
+
+
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+// Routes
+require('./routes/web')(app);
+
+
+app.listen(PORT,()=>{
+    console.log(`app is listening on port : ${PORT}`);
+})
